1aabb4cf092bd976970a76ad08039594c00da2da
Abdul Hamid <abdul.hamid.kol8@gmail.com>
2019-07-07 17:23:41 +0530

    Implemented Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/home-page.component';
 import ShopPage from './pages/shoppage/shop-page.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
